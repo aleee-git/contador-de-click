df05c8304f22eab2e4c82eee2bd97026c112366e
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-01-09 21:38:49 -0600

    Importar Imagen y CSS details

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import logoCat from './Images/Logo.webp';
 
 function App() {
   return (
     <div className="App">
+      <div className="logo-container">
+        <img className="logo" src={logoCat} alt="Logo"/>
+      </div>
 
     </div>
   );
