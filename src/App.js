b034e6071ba504839a932680dbb32380cf2d047f
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-01-09 22:33:29 -0600

    Funcionalidad de Botones

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
+import Boton from './Componentes/Boton';
 import logoCat from './Images/Logo.webp';
 
 function App() {
+
+  /* Definir funcion flecha JS 
+  Definir los parametros en ()
+  Retornar ponerlo en {} */
+  const manejarClick = () => {
+    console.log("Si funciona esta shit :)");
+  }
+
+  const reiniciarcontador = () => {
+    console.log("Si funciona esta shit :)");
+  }
+  
+
   return (
     <div className="App">
       <div className="logo-container">
         <img className="logo" src={logoCat} alt="Logo"/>
       </div>
+      <div className="main-container">
+      <Boton
+      texto="Clic" 
+      esBotonClick={true} 
+      manejarClick={manejarClick} />
 
+      <Boton
+      texto="Reiniciar" 
+      esBotonClick={false} 
+      manejarClick={reiniciarcontador} />
+
+      </div>
     </div>
   );
 }
