c088ec48182c2a8130a3d69e9fee47e1478263b2
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-01-09 22:01:55 -0600

    Crear componentes y Definir Props

---
+++ src/Componentes/Boton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/* Agregar las 3 props de la funcion */
+function Boton ({texto, esBotonDeClick, manejarClick}) {
+    return (
+        /* Agregar props al button
+        esBotonDeClick es un operador ternario para saber la condicion del boton
+        Si es boton de click se asigna la clase "boton-click"
+        Sino se asigna la clase "boton-reiniciar" */
+        <button className={ esBotonDeClick ? "boton-click" : "boton-reiniciar" } onClick={manejarClick}>{texto}</button>
+    );
+}
+
+/* Exportar boton */
+export default Boton;
